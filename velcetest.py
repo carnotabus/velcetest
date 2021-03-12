3a51a8b9a4e0c6df3362f6045a1d065b4ae1d1ea
Lac0lm3n4 <74153596+Lac0lm3n4@users.noreply.github.com>
2021-03-11 23:00:48 -0300

    Add files via upload

---
+++ velcetest.py
@@ -0,0 +1,279 @@
+
+import os
+
+print("""
+
+\t  __          __                         
+\t / /____ ___ / /_                        
+\t/ __/ -_|_-</ __/                        
+\t\__/\__/___/\__/     ___                 
+\t _  _____ / /______ / _/__  ___  ___ ____
+\t| |/ / -_) / __/ -_) _/ _ \/ _ \/ _ `(_-<
+\t|___/\__/_/\__/\__/_/ \___/_//_/\_,_/___/
+""")
+
+print("""
+\tJuego test velcefonas!! este juego se 
+\ttrata de 10 preguntas relacionado a 
+\tvelcefonas... tendras 3 alternativas
+\ty cada respuesta buena es 1 punto, luego
+\tcomparate con la tabla! :D""")
+    
+nombre = input("\n\tbienvenido, ingresa tu nickname para comenzar: ")
+
+os.system("clear")
+
+print(f"\n¡Hola {nombre}! estas dentro del juego \nde vulkano 'test velcefonas',contaras con 10 preguntas por ahora.\nel juego consiste en que deberas responder \nde manera correcta seleccionando una opción.\ncada pregunta se vuelve más dificil, suerte!")
+
+comienzo = int(input("\n¿Empezamos? \n[1]SI \n[2]NO \nalternativa: "))
+
+if comienzo == 1:
+    print("wena")
+    
+while comienzo >= 2:
+ print("""
+    man, no seas aburrido xd
+    """)
+
+ comienzo = int(input("""EMPEZAMOS? \n[1]SI \n[2]NO \nalternativa: """))  
+
+os.system("clear")
+
+print("\n\tNIVEL 1")
+
+op1 = int(input("\n\t¿Quien creo velcefonas? \n\t[1]vulk4n0 \n\t[2]blood \n\t[3]vulk4n0 y blood \n\talternativa: "))
+
+if op1 == 1:
+    print("\n\tExacto, solo vulk4n0 inspirado \n\tpor su anterior grupo 'narcos altos' \n\tforjo lo que en su tiempo era cartel velcefonas (1 punto)")
+else:
+    print("\n\tpuede ser tentador la idea que blood \n\ty vulk4n0 lo forjaron, pero no.(0 puntos)")
+
+comienzo = int(input("\nDeseas continuar al nivel 2? \n[1]SI \n[2]NO \nalternativa: "))
+
+if comienzo == 1:
+    print("wena")
+                                                                       #aca simplemente es continuar y luego borrar.
+while comienzo >= 2:
+ print("""
+    dale pajero, si te gusta jugar xd
+    """)
+
+ comienzo = int(input("¿Deseas continuar al nivel 2? \n[1]SI \n[2]NO \nalternativa: ")) 
+
+os.system("clear")    
+
+print("\n\tNIVEL 2")
+
+op1 = int(input("\n\t¿Quien fue el más bueno del grupo? \n\t[1]vulk4n0 \n\t[2]NHT \n\t[3]Akyles \n\talternativa: "))
+
+if op1 == 3:
+    print("\n\tExacto, akyles le enseño a vulk4n0 \n\ty vulk4n0 a la mayoria del grupo.  (1 punto)")
+else:
+    print("\n\tSe podria pensar que vulk4n0 fue la bestia. \n\tPero en ese tiempo el era muy noob, y akyles le enseño.(0 puntos)")
+
+comienzo = int(input("\nDeseas continuar al nivel 3? \n[1]SI \n[2]NO \nalternativa: "))
+
+if comienzo == 1:
+    print("wena")
+                                                                       #aca simplemente es continuar y luego borrar.
+while comienzo >= 2:
+ print("""
+    dale pajero, si te gusta jugar xd
+    """)
+
+ comienzo = int(input("¿Deseas continuar al nivel 3? \n[1]SI \n[2]NO \nalternativa: ")) 
+
+os.system("clear")     
+
+#ESTE PEDAZO DE CODIGO SE REPITE
+
+print("\n\tNIVEL 3")
+
+op1 = int(input("\n\t¿Cuantos miembros llego a tener el grupo maximo? \n\t[1]60 \n\t[2]45 \n\t[3]30 \n\talternativa: "))
+
+if op1 == 1:
+    print("\n\tcorrecto, 60 fue el maximo. (1 punto)")
+else:
+    print("\n\tIncorrecto, 60 fue el maximo de integrantes activos(0 puntos)")    
+
+comienzo = int(input("\n¿Deseas continuar al nivel 4? \n[1]SI \n[2]NO \nalternativa: "))
+
+if comienzo == 1:
+    print("wena")
+                                                                       #aca simplemente es continuar y luego borrar.
+while comienzo >= 2:
+ print("""
+    dale pajero, si te gusta jugar xd
+    """)
+
+ comienzo = int(input("¿Deseas continuar al nivel 4? \n[1]SI \n[2]NO \nalternativa: ")) 
+
+os.system("clear") 
+
+# ESTE PEDAZO DE CODIGO SE REPITE
+
+print("\n\tNIVEL 4")
+
+op1 = int(input("\n\t¿Cuanto tiempo duro la infiltración más larga a un grupo? \n\t[1]1 semana \n\t[2]2 meses \n\t[3]1 mes \n\talternativa: "))
+
+if op1 == 3:
+    print("\n\tcorrecto, un usuario demoro 1 mes en infiltración (1 punto)")
+else:
+    print("\n\tIncorrecto, el maximo fue 1 mes.(0 puntos)")    
+
+comienzo = int(input("\n¿Deseas continuar al nivel 5? \n[1]SI \n[2]NO \nalternativa: "))
+
+if comienzo == 1:
+    print("wena")
+                                                                       #aca simplemente es continuar y luego borrar.
+while comienzo >= 2:
+ print("""
+    dale pajero, si te gusta jugar xd
+    """)
+
+ comienzo = int(input("¿Deseas continuar al nivel 5? \n[1]SI \n[2]NO \nalternativa: ")) 
+
+os.system("clear") 
+
+# ESTE PEDAZO DE CODIGO SE REPITE
+
+print("\n\tNIVEL 5")
+
+op1 = int(input("\n\t¿Y quien fue el que se infiltro? \n\t[1]vulk4n0 \n\t[2]jerzy \n\t[3]peppo \n\talternativa: "))
+
+if op1 == 3:
+    print("\n\tcorrecto, fue peppo, 1 mes de inflitrado. (1 punto)")
+else:
+    print("\n\tIncorrecto, jerzy y vulk4n0 solo dirigieron la infiltracion. (0 puntos)")    
+
+comienzo = int(input("\n¿Deseas continuar al nivel 6? \n[1]SI \n[2]NO \nalternativa: "))
+
+if comienzo == 1:
+    print("wena")
+                                                                       #aca simplemente es continuar y luego borrar.
+while comienzo >= 2:
+ print("""
+    dale pajero, si te gusta jugar xd
+    """)
+
+ comienzo = int(input("¿Deseas continuar al nivel 6? \n[1]SI \n[2]NO \nalternativa: ")) 
+
+os.system("clear") 
+
+# ESTE PEDAZO DE CODIGO SE REPITE
+print("\n\tNIVEL 6")
+
+op1 = int(input("\n\t¿Quien fue el enemigo más dificil de vencer? \n\t[1]Beba army \n\t[2]Cartel Perú \n\t[3]mushroom \n\talternativa: "))
+
+if op1 == 1:
+    print("\n\tRealmente solo fue beba army, ya \nque no se destruyeron a todos (1 punto)")
+else:
+    print("\n\tNah,mushroom y cartel perú morian facil(0 puntos)")    
+
+comienzo = int(input("\n¿Deseas continuar al nivel 7? \n[1]SI \n[2]NO \nalternativa: "))
+
+if comienzo == 1:
+    print("wena")
+                                                                       #aca simplemente es continuar y luego borrar.
+while comienzo >= 2:
+ print("""
+    dale pajero, si te gusta jugar xd
+    """)
+
+ comienzo = int(input("¿Deseas continuar al nivel 7? \n[1]SI \n[2]NO \nalternativa: ")) 
+
+os.system("clear") 
+
+# ESTE PEDAZO DE CODIGO SE REPITE
+print("\n\tNIVEL 7")
+
+op1 = int(input("\n\t¿Quien era el elegido a superar a vulk4n0?\n\t[1]Jean \n\t[2]NHT \n\t[3]RASEK \n\talternativa: "))
+
+if op1 == 3:
+    print("\n\tRealmente solo fue Rasek, \npero se puso re gil (1 punto)")
+else:
+    print("\n\tNht pudo superar a vulk4no, \npero no era el elegido.(0 puntos)")    
+
+comienzo = int(input("\n¿Deseas continuar al nivel 8? \n[1]SI \n[2]NO \nalternativa: "))
+
+if comienzo == 1:
+    print("wena")
+                                                                       #aca simplemente es continuar y luego borrar.
+while comienzo >= 2:
+ print("""
+    dale pajero, si te gusta jugar xd
+    """)
+
+ comienzo = int(input("¿Deseas continuar al nivel 8? \n[1]SI \n[2]NO \nalternativa: ")) 
+
+os.system("clear") 
+
+# ESTE PEDAZO DE CODIGO SE REPITE
+print("\n\tNIVEL 8")
+
+op1 = int(input("\n\t¿Quien fue el enemigo más importante de velcefonas?\n\t[1]mushroom \n\t[2]piero \n\t[3]Los peruanos XD \n\talternativa: "))
+
+if op1 == 2:
+    print("\n\tPIERO, definitivamente el perro de piero. (1 punto)")
+else:
+    print("\n\tLos peruanos y los putos de mushroom fueron enemigos \n pero eran segundo plano.(0 puntos)")    
+
+comienzo = int(input("\n¿Deseas continuar al nivel 9? \n[1]SI \n[2]NO \nalternativa: "))
+
+if comienzo == 1:
+    print("wena")
+                                                                       #aca simplemente es continuar y luego borrar.
+while comienzo >= 2:
+ print("""
+    dale pajero, si te gusta jugar xd
+    """)
+
+ comienzo = int(input("¿Deseas continuar al nivel 9? \n[1]SI \n[2]NO \nalternativa: ")) 
+
+os.system("clear") 
+
+# ESTE PEDAZO DE CODIGO SE REPITE
+print("\n\tNIVEL 9")
+
+op1 = int(input("\n\t¿Velcefonas alguna vez fue vencido??\n\t[1] 1 vez solamente\n\t[2]Una vez casi \n\t[3]Jamas \n\talternativa: "))
+
+if op1 == 3:
+    print("\n\tJAMAS!, velcefonas jamas cayo. (1 punto)")
+else:
+    print("\n\tINCORRECTO XD, vulk4n0 jamas permitio eso. (0 puntos)")    
+
+comienzo = int(input("\n¿Deseas continuar al nivel 10? \n[1]SI \n[2]NO \nalternativa: "))
+
+if comienzo == 1:
+    print("wena")
+                                                                       #aca simplemente es continuar y luego borrar.
+while comienzo >= 2:
+ print("""
+    dale pajero, si te gusta jugar xd
+    """)
+
+ comienzo = int(input("¿Deseas continuar al nivel 10? \n[1]SI \n[2]NO \nalternativa: ")) 
+
+os.system("clear") 
+
+# ESTE PEDAZO DE CODIGO SE REPITE
+print("\n\tNIVEL 10")
+
+op1 = int(input("\n\t¿Que velcefonas es más fuerte?\n\t[1] Dragon velcefonas\n\t[2]Velcefonas black \n\t[3]Velcefonas ORIGINAL \n\t[4]Velcefonas Actual \n\talternativa: "))
+
+if op1 == 4:
+    print("\n\t Definitivamente ahora, el nivel es más potente. (1 punto)")
+else:
+    print("\n\tBlack fue una masacre, original fue el más activo y dragon fue el más debil...\nPero el actual si se lo propone, destruye lo que quiere. (0 puntos)")    
+
+comienzo = int(input("\n¿Te gusto? Se vendran más actualizaciones. \n[1]SI \n[2]NO \nalternativa: "))
+
+if comienzo == 1:
+    print("\nGracias por jugar")
+                                                                       #aca simplemente es continuar y luego borrar.
+elif comienzo >= 2:
+ print("""
+    JOdete gay
+    """)
+
+# ESTE PEDAZO DE CODIGO SE REPITE
